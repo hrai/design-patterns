181648a0b988f029b068c5ae627eb8e22ef96ee1
Hangjit Rai <rai.hangjit@gmail.com>
2018-12-12 13:25:02 +1100

    Created Builder Project

---
+++ Builder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesignPatterns.Builder
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var mealBuilder = new MealBuilder()
+               .BuildEntree("spring rolls")
+               .BuildMains("pad thai")
+               .BuildDrinks("coke")
+               .BuildDessert("mousse");
+
+           var meal = mealBuilder.GetMeal();
+           Console.WriteLine(((Meal)meal).Entree);
+        }
+    }
+}
