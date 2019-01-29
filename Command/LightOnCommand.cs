7e144467a58f83cf90c2ea2648c50909885b383e
Hangjit Rai <rai.hangjit@gmail.com>
2019-01-29 23:11:14 +1100

    Lightoncommand Added

---
+++ Command/LightOnCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Command
+{
+    class LightOnCommand
+    {
+        private Light _light;
+
+        public LightOnCommand(Light light)
+        {
+            _light = light;
+        }
+
+        public void Execute()
+        {
+            _light.on();
+        }
+    }
+}
