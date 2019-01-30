7d3efb7552220b266b94ecc92d722bfac53182f7
Hangjit Rai <rai.hangjit@gmail.com>
2019-01-30 20:41:50 +1100

    Added Button Pressed Cmd To Program

--- Command/Program.cs
+++ Command/Program.cs
@@ -7,6 +7,14 @@ namespace Command
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var remote =new SimpleRemoteControl();
+
+            var light = new Light();
+            var cmd = new LightOnCommand(light);
+            remote.SetCommand(cmd);
+
+            remote.ButtonPressed();
         }
     }
 }
