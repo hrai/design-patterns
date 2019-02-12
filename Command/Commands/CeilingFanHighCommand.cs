6649df07878a8abc4bd6aa0f9f87ee60a1b492b0
Hangjit Rai <rai.hangjit@gmail.com>
2019-02-12 22:28:45 +1100

    Added Ceiling Fan On Cmd

---
+++ Command/Commands/CeilingFanHighCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Command
+{
+    public class CeilingFanOnCommand:ICommand
+    {
+        private CeilingFan _ceilingFan;
+
+        public CeilingFanOnCommand(CeilingFan ceilingFan)
+        {
+            _ceilingFan = ceilingFan;
+        }
+
+        public void Execute()
+        {
+            _ceilingFan.On();
+        }
+    }
+}
