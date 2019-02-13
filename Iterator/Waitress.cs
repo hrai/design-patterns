6420287293aae9d8dca353f495241efea36a99be
Hangjit Rai <rai.hangjit@gmail.com>
2019-02-13 23:15:13 +1100

    Cleanup

--- Iterator/Waitress.cs
+++ Iterator/Waitress.cs
@@ -11,17 +11,16 @@ namespace Iterator
         {
             this.menuIterators = menuIterators;
         }
-        
+
         public void printMenuItems()
         {
             foreach(var menuIterator in menuIterators)
             {
                 System.Console.WriteLine("Printing menu items...");
-            
+
                 while(menuIterator.MoveNext())
                 {
                     var currentItem = menuIterator.Current;
-
                     Console.WriteLine($"# {menuIterator.GetType().Name} : {currentItem.Name}");
                 }
             }
