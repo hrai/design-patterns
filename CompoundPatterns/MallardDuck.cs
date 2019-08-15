04c0f605e024c97b87373d5fca5170dac8c46862
Hangjit Rai <rai.hangjit@gmail.com>
2019-08-15 11:56:10 +1000

    Added Ducks

---
+++ CompoundPatterns/MallardDuck.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CompoundPatterns
+{
+    public class MallardDuck : IQuackable
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Quack");
+        }
+    }
+}
---
+++ CompoundPatterns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CompoundPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sim = new DuckSimulator();
+            sim.Simulate();
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class DuckSimulator
+    {
+        internal void Simulate()
+        {
+            Console.WriteLine("Simulating quacking...");
+
+            var mallardDuck = new MallardDuck();
+            var redheadDuck = new RedheadDuck();
+            var duckCall = new DuckCall();
+            var rubberDuck = new RubberDuck();
+
+            Simulate(mallardDuck);
+            Simulate(redheadDuck);
+            Simulate(duckCall);
+            Simulate(rubberDuck);
+        }
+
+        private void Simulate(IQuackable duck)
+        {
+            duck.Quack();
+        }
+    }
+}
